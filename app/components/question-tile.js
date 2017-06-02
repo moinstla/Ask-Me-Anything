bcf44fc86aff36d0e77c6e1120b05287b5f8e41a
Monique St. Laurent <moinstla@gmail.com>
2017-06-02 15:11:37 -0700

    add delete and new question functionality

--- app/components/question-tile.js
+++ app/components/question-tile.js
@@ -1,9 +1,9 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
-  actions: {
+actions: {
     delete(question) {
-      if (confirm('Are you sure you want to delete this question?')) {
+    if (confirm('Are you sure you want to delete this question?')) {
         this.sendAction('destroyQuestion', question);
       }
     }
--- app/routes/index.js
+++ app/routes/index.js
@@ -2,9 +2,9 @@ import Ember from 'ember';
 
 export default Ember.Route.extend({
   model() {
-    return this.store.findAll('question');
-  },
-
+  return this.store.findAll('question');
+},
+  //
   actions: {
     saveQuestion(params) {
      var newQuestion = this.store.createRecord('question', params);
